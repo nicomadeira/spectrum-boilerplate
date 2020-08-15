ef56468ce9d1ba29f39acffc8299947a62366acf
Nicolle Madeira <nicollemguimaraes@gmail.com>
2020-08-15 20:05:07 -0300

    implement storybook controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,9 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
   title: 'Main',
-  component: Main,
-  decorators: [withKnobs]
+  component: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    description={text(
-      'Description',
-      'Starter project with Next, React and Typescript!'
-    )}
-    title={text('Title', 'Spectrum Boilerplate')}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
